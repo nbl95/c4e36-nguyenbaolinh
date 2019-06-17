fcfe0cd00c8839e1fa162f36e7d54f0b6f53f654
nbl95 <nbl.ltu12@gmail.com>
2019-06-17 22:05:55 +0700

    abcd

---
+++ session1/index.js
@@ -0,0 +1,41 @@
+// // const x = Number(prompt("Enter X"));
+// // const y = Number(prompt("Enter Y"));
+
+// // console.log(x+y);
+
+// // loop
+
+// // cooment code
+
+// for(let y = 0; y < 5; y++){
+//     console.log(y)
+//     console.log("hihi");
+// }
+
+
+// bt1
+for (let index = 1; index < 101; index++) {
+     console.log(index);
+     
+    
+}
+
+
+// // bt2
+for (let index = 1; index < 101; index+=5) {
+    console.log(index);
+
+
+}
+
+
+// // bt3
+for (let index = 100; index > 0; index--) {
+    console.log(index);
+}
+
+
+// bt4
+for (let index = 100; index > 0; index-=3) {
+    console.log(index);
+}
